f58a0a0e61d8fc5c6c1a48c84fc780e66abdd836
nazimuddin <nazimuddin102001@gmail.com>
2024-08-27 19:06:21 +0600

    completed parent and teacher page design

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,4 +1,4 @@
-import { menuItems } from '@/data';
+import { menuItems, role } from '@/data';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -11,16 +11,18 @@ const Menu = () => {
             {item.title}
           </span>
           {item.items.map((subItem) => {
-            return (
-              <Link
-                href={subItem.href}
-                key={subItem.label}
-                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
-              >
-                <Image src={subItem.icon} alt="" width={20} height={20} />
-                <span className="hidden lg:block">{subItem.label}</span>
-              </Link>
-            );
+            if (subItem.visible.includes(role)) {
+              return (
+                <Link
+                  href={subItem.href}
+                  key={subItem.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-nazimSkyLight"
+                >
+                  <Image src={subItem.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{subItem.label}</span>
+                </Link>
+              );
+            }
           })}
         </div>
       ))}
