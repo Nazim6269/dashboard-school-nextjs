84a5a28dd563772346a1ba392147c145426989e9
nazimuddin <nazimuddin102001@gmail.com>
2024-08-27 21:07:27 +0600

    completed student and teacher single page

--- components/FormModal.tsx
+++ components/FormModal.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-
-const FormModal = () => {
+const FormModal = ({
+  table,
+  type,
+  data,
+}: {
+  table?: string;
+  type?: string;
+  data?: {};
+}) => {
   return <div>FormModal</div>;
 };
 
