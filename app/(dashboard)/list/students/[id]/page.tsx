f70e813dfb5bfd31437942ee7a2960f40da357a7
Nazim Uddin <nazimuddin102001@gmail.com>
2024-11-26 19:40:49 +0600

    implemented exam sections data fetching and search

--- app/(dashboard)/list/students/[id]/page.tsx
+++ app/(dashboard)/list/students/[id]/page.tsx
@@ -130,13 +130,22 @@ const SingleStudentPage = () => {
             >
               Student&apos;s Teachers
             </Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">
+            <Link
+              className="p-3 rounded-md bg-pink-50"
+              href={`/list/exams?classId=${2}`}
+            >
               Student&apos;s Exams
             </Link>
-            <Link className="p-3 rounded-md bg-nazimSkyLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-nazimSkyLight"
+              href={`/list/assignments?classId=${2}`}
+            >
               Student&apos;s Assignments
             </Link>
-            <Link className="p-3 rounded-md bg-nazimYellowLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-nazimYellowLight"
+              href={`/list/results?classId=${2}`}
+            >
               Student&apos;s Results
             </Link>
           </div>
